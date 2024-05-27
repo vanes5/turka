b1d412cd83707a4d093d405357a04869dc379b72
BBeenniii <hollobeni20@gmail.com>
2024-05-27 15:33:56 +0200

    Login - Design

--- turkalo/src/comps/Profile.tsx
+++ turkalo/src/comps/Profile.tsx
@@ -1,6 +1,9 @@
 import { auth , googleProvider} from "../firebase";
 import { createUserWithEmailAndPassword,signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
+import '../CSS/Profile.css';
+import React from 'react';
+import { Card, CardBody, CardTitle, Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
 
 export const Profile = () => {
   const [email, setEmail] = useState("");
@@ -28,16 +31,45 @@ export const Profile = () => {
     }
   };
   return (
-    <div>
-      <input placeholder="Email.." onChange={(e) => setEmail(e.target.value)} />
-      <input
-        type="password"
-        placeholder="Password.."
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={signIn}> Signin</button>
-      <button onClick={signInWithGoogle}> Signin with google</button>
-      <button onClick={logOut}> logOut</button>
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <Card>
+            <CardBody>
+              <CardTitle tag="h5">Login</CardTitle>
+              <Form>
+                <FormGroup>
+                  <Label for="emailInput">Email</Label>
+                  <Input
+                    type="email"
+                    id="emailInput"
+                    placeholder="Email.."
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="passwordInput">Password</Label>
+                  <Input
+                    type="password"
+                    id="passwordInput"
+                    placeholder="Password.."
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <Container className="mt-5">
+                <Row className="justify-content-center">
+                  <Col md={6}>
+                    <Button color="primary" block onClick={signIn} className="bold-text" style={{ width: '50%'  }}>Signin</Button>
+                    <Button color="danger" block onClick={signInWithGoogle} className="mt-2 bold-text" style={{ width: '50%' }}>Signin with Google</Button>
+                    <Button color="secondary" block onClick={logOut} className="mt-2 bold-text" style={{ width: '50%' }}>Log Out</Button>
+                  </Col>
+                </Row>
+              </Container>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
