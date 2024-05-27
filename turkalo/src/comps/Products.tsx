0d0b8ce1ab8522f73c44103b6ebc641c3795f761
BBeenniii <hollobeni20@gmail.com>
2024-05-27 18:07:26 +0200

    KÁRTYA

--- turkalo/src/comps/Products.tsx
+++ turkalo/src/comps/Products.tsx
@@ -1,20 +1,13 @@
 import { Container, Image, Row, Col } from 'react-bootstrap';
-import  bg from '../img/bg.png';
+import '../CSS/Produtcs.css';
 
 export function Products() {
     return (
       <>
       <Container>
         <Row>
-          <Col>      
-            <Image src={bg} roundedCircle width="20%"/>
-          </Col>
-          <Col>      
-            <h1>VöcsökWebShop</h1>
-          </Col>
         </Row>
         <Row id='termek'>
-
         </Row>
       </Container>
       </>
