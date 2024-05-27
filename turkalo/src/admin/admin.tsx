75f991d7334c68ebc91bead279785582aff783c3
bnolod <sadfoolc@gmail.com>
2024-05-27 16:43:07 +0200

    termek mutatas

--- turkalo/src/admin/admin.tsx
+++ turkalo/src/admin/admin.tsx
@@ -19,6 +19,9 @@ export function AdminPage() {
                 <input type="file" name="file" />
                 <button type="submit" onClick={submitForm}>upload</button>
             </form>
+
+
+            <div id="products"></div>
         </div>
     )
 }
@@ -96,3 +99,41 @@ function submitForm() {
 
 }
 
+
+async function getProducts() {
+  
+
+    const db = getDatabase();
+    const clothesRef = ref(db, 'clothes');
+    console.log(clothesRef)
+    onValue(clothesRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data)
+      for (const key in data) {
+        const product = data[key];
+        console.log(product)
+        const storage = getStorage();
+        const storageRef = sref(storage, 'images/' + product.imageUrl);
+        const imageUrl = getDownloadURL(storageRef)
+        imageUrl.then((url) => {
+        
+        const productDiv = document.createElement('div');
+        productDiv.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.desc}</p>
+          <p class="card-text">${product.price} Ft</p>
+          <a href="#" class="btn btn-primary">Buy</a>
+        </div>
+      </div>
+        `
+        document.getElementById('termek')?.appendChild(productDiv)
+  
+      }
+    )}
+  })  
+  }
+  
+  getProducts()
--- turkalo/src/comps/Products.tsx
+++ turkalo/src/comps/Products.tsx
@@ -13,7 +13,54 @@ export function Products() {
             <h1>VöcsökWebShop</h1>
           </Col>
         </Row>
+        <Row id='termek'>
+
+        </Row>
       </Container>
       </>
     )
   }
+
+
+
+import { getDatabase, ref, onValue } from "firebase/database";
+import { getStorage, ref as sref,getDownloadURL} from "firebase/storage";
+
+
+async function getProducts() {
+  
+
+  const db = getDatabase();
+  const clothesRef = ref(db, 'clothes');
+  console.log(clothesRef)
+  onValue(clothesRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+    for (const key in data) {
+      const product = data[key];
+      console.log(product)
+      const storage = getStorage();
+      const storageRef = sref(storage, 'images/' + product.imageUrl);
+      const imageUrl = getDownloadURL(storageRef)
+      imageUrl.then((url) => {
+      
+      const productDiv = document.createElement('div');
+      productDiv.innerHTML = `
+      <div class="card" style="width: 18rem;">
+      <img src="${url}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${product.name}</h5>
+        <p class="card-text">${product.desc}</p>
+        <p class="card-text">${product.price} Ft</p>
+        <a href="#" class="btn btn-primary">Buy</a>
+      </div>
+    </div>
+      `
+      document.getElementById('termek')?.appendChild(productDiv)
+
+    }
+  )}
+})  
+}
+
+getProducts()
