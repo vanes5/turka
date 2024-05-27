82e43436a9f035c30d72183e03d88ac98b9d4a89
BBeenniii <hollobeni20@gmail.com>
2024-05-27 13:48:14 +0200

    Fo

--- turkalo/src/comps/Home.tsx
+++ turkalo/src/comps/Home.tsx
@@ -1,18 +1,19 @@
 import { Container, Image, Row, Col } from 'react-bootstrap';
-import  bg from '../img/bg.png';
+import bg from '../img/bg.png';
+import '../CSS/Home.css'; 
 
 export function Home() {
     return (
       <>
-      <Container>
-        <Row>     
+      <Container className="home-container">
+        <Row className="align-items-center">     
           <Col>      
-            <Image src={bg} height="25%" width="100%"/>
-            <h1 className="text-center">VöcsökWebShop</h1>
-            <p className='text-justify'>Üdvözöljük a Vöcsök online turkálóban, ahol a stílus és a fenntarthatóság találkozik! Webshopunkban különleges, egyedi ruhadarabok és kiegészítők széles választékát találja, melyeket gondosan válogattunk össze a legjobb minőségű second-hand termékek közül. Nálunk mindenki találhat valami különlegeset, legyen szó vintage kincsekről, modern dizájnerekről vagy klasszikus alapdarabokról. Célunk, hogy fenntarthatóbbá tegyük a divatot, miközben vásárlóinknak egyedi és megfizethető lehetőségeket kínálunk. Böngésszen kínálatunkban, és találja meg új kedvenceit a Vöcsök online turkálóban</p>
+            <Image src={bg} className="home-image" />
+            <h1 className="text-center home-title">VöcsökWebShop</h1>
+            <p className='text-justify home-text'>Üdvözöljük a Vöcsök online turkálóban, ahol a stílus és a fenntarthatóság találkozik! Webshopunkban különleges, egyedi ruhadarabok és kiegészítők széles választékát találja, melyeket gondosan válogattunk össze a legjobb minőségű second-hand termékek közül. Nálunk mindenki találhat valami különlegeset, legyen szó vintage kincsekről, modern dizájnerekről vagy klasszikus alapdarabokról. Célunk, hogy fenntarthatóbbá tegyük a divatot, miközben vásárlóinknak egyedi és megfizethető lehetőségeket kínálunk. Böngésszen kínálatunkban, és találja meg új kedvenceit a Vöcsök online turkálóban</p>
           </Col>
         </Row>
       </Container>
       </>
     )
-  }
+}
